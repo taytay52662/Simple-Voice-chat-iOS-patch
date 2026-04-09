e5bfc38c8c2b154fa1c368824e3af3727518d635
The big P (pearman) <taylenwells2012@icloud.com>
2026-04-09 07:38:09 +0000

    attempt to fix it loading

--- src/main/java/de/maxhenkel/voicechat/mixin/MicrophoneManagerMixin.java
+++ src/main/java/de/maxhenkel/voicechat/mixin/MicrophoneManagerMixin.java
@@ -15,23 +15,10 @@ public class MicrophoneManagerMixin {
     private static void injectIOSMicrophone(CallbackInfoReturnable<Microphone> cir) throws MicrophoneException {
         if (!AudioQueueMicrophone.isAvailable()) return;
         String deviceName = null;
-        try {
-            Object config = Class.forName("de.maxhenkel.voicechat.VoicechatClient").getField("CLIENT_CONFIG").get(null);
-            Object entry = config.getClass().getField("microphone").get(config);
-            Object value = entry.getClass().getMethod("get").invoke(entry);
-            if (value instanceof String s && !s.isBlank()) deviceName = s;
-        } catch (Throwable ignored) {}
+        
         int sampleRate = 48000;
         int bufferSize = 960;
-        try {
-            MicrophoneManager self = (MicrophoneManager)(Object) this;
-            Field srField = self.getClass().getDeclaredField("sampleRate");
-            srField.setAccessible(true);
-            sampleRate = srField.getInt(self);
-            Field bsField = self.getClass().getDeclaredField("bufferSize");
-            bsField.setAccessible(true);
-            bufferSize = bsField.getInt(self);
-        } catch (Throwable ignored) {}
+        
         AudioQueueMicrophone mic = new AudioQueueMicrophone(sampleRate, bufferSize, deviceName);
         mic.open();
         cir.setReturnValue(mic);
