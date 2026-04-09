6784d2d3a5cf6fbe215bb32cb46da8a037c26ff8
The big P (pearman) <taylenwells2012@icloud.com>
2026-04-09 09:20:20 +0000

    force use IOS mic

--- src/main/java/de/maxhenkel/voicechat/mixin/MicrophoneManagerMixin.java
+++ src/main/java/de/maxhenkel/voicechat/mixin/MicrophoneManagerMixin.java
@@ -4,26 +4,29 @@ import de.maxhenkel.voicechat.ios.AudioQueueMicrophone;
 import de.maxhenkel.voicechat.voice.client.microphone.Microphone;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(targets = "de.maxhenkel.voicechat.voice.client.microphone.MicrophoneManager")
 public class MicrophoneManagerMixin {
 
-    @Inject(method = "createMicrophone", at = @At("HEAD"), cancellable = true)
-    private static void injectIOSMicrophone(CallbackInfoReturnable<Microphone> cir) {
+    @Redirect(
+        method = "*",
+        at = @At(
+            value = "NEW",
+            target = "de/maxhenkel/voicechat/voice/client/microphone/OpenALMicrophone"
+        )
+    )
+    private Microphone redirectMicrophone(int sampleRate, int bufferSize, String device) {
         try {
-            if (!AudioQueueMicrophone.isAvailable()) return;
-
             AudioQueueMicrophone mic =
-                    new AudioQueueMicrophone(48000, 960, null);
+                new AudioQueueMicrophone(48000, 960, null);
 
             mic.open();
             mic.start();
 
-            cir.setReturnValue(mic);
-            cir.cancel();
-
-        } catch (Throwable ignored) {}
+            return mic;
+        } catch (Throwable t) {
+            throw new RuntimeException(t);
+        }
     }
 }
