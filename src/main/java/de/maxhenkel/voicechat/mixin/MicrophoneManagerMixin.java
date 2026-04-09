558e2443a6f90ab3e622bc2dfe16de64fa43b7e0
The big P (pearman) <taylenwells2012@icloud.com>
2026-04-09 09:45:40 +0000

    we will get this mixin to work trust

--- src/main/java/de/maxhenkel/voicechat/mixin/MicrophoneManagerMixin.java
+++ src/main/java/de/maxhenkel/voicechat/mixin/MicrophoneManagerMixin.java
@@ -4,19 +4,23 @@ import de.maxhenkel.voicechat.ios.AudioQueueMicrophone;
 import de.maxhenkel.voicechat.voice.client.microphone.Microphone;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(targets = "de.maxhenkel.voicechat.voice.client.microphone.MicrophoneManager")
 public class MicrophoneManagerMixin {
 
-    @Redirect(
-        method = "*",
-        at = @At(
-            value = "NEW",
-            target = "de/maxhenkel/voicechat/voice/client/microphone/OpenALMicrophone"
-        )
+    @Inject(
+        method = "createMicrophone",
+        at = @At("HEAD"),
+        cancellable = true,
+        remap = false
     )
-    private Microphone redirectMic(int sampleRate, int bufferSize, String device) {
-        return new AudioQueueMicrophone(sampleRate, bufferSize, device);
+    private void createIOSMic(CallbackInfoReturnable<Microphone> cir) {
+        try {
+            cir.setReturnValue(new AudioQueueMicrophone(48000, 960, null));
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
     }
 }
