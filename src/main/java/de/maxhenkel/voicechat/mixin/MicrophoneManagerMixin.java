cc2187e0823f84a4e905a752676cd26b4ea54c31
The big P (pearman) <taylenwells2012@icloud.com>
2026-04-11 23:48:12 +0000

    same thing

--- src/main/java/de/maxhenkel/voicechat/mixin/MicrophoneManagerMixin.java
+++ src/main/java/de/maxhenkel/voicechat/mixin/MicrophoneManagerMixin.java
@@ -1,31 +1,23 @@
-
 package de.maxhenkel.voicechat.mixin;
 
 import de.maxhenkel.voicechat.ios.AudioQueueMicrophone;
+import de.maxhenkel.voicechat.voice.client.MicrophoneException;
 import de.maxhenkel.voicechat.voice.client.microphone.Microphone;
 import de.maxhenkel.voicechat.voice.client.microphone.MicrophoneManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(value = MicrophoneManager.class, remap = false)
 public class MicrophoneManagerMixin {
 
-    @Inject(
-        method = "createMicrophone()Lde/maxhenkel/voicechat/voice/client/microphone/Microphone;",
-        at = @At("HEAD"),
-        cancellable = true,
-        remap = false
-    )
-    private static void injectIOSMicrophone(CallbackInfoReturnable<Microphone> cir) {
-        if (!AudioQueueMicrophone.isAvailable()) return;
-        try {
-            AudioQueueMicrophone mic = new AudioQueueMicrophone(48000, 960, null);
-            mic.open();
-            cir.setReturnValue(mic);
-        } catch (RuntimeException e) {
-            // Fall through to original if AudioQueue fails
-        }
+    /**
+     * @author voicechat-ios
+     * @reason Replace microphone creation with iOS AudioQueue implementation
+     */
+    @Overwrite(remap = false)
+    public Microphone createMicrophone() throws MicrophoneException {
+        AudioQueueMicrophone mic = new AudioQueueMicrophone(48000, 960, null);
+        mic.open();
+        return mic;
     }
 }
