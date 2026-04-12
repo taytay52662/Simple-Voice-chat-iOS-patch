99c54d84a5cd54f512ef6cb433cff660d1a3c9a9
The big P (pearman) <taylenwells2012@icloud.com>
2026-04-12 00:00:25 +0000

    strip down MicrophoneManagerMixin even more

--- src/main/java/de/maxhenkel/voicechat/mixin/MicrophoneManagerMixin.java
+++ src/main/java/de/maxhenkel/voicechat/mixin/MicrophoneManagerMixin.java
@@ -9,18 +9,10 @@ import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(value = MicrophoneManager.class, remap = false)
 public class MicrophoneManagerMixin {
-
-```
-/**
- * @author voicechat-ios
- * @reason Replace microphone creation with iOS AudioQueue implementation
- */
 @Overwrite(remap = false)
 public static Microphone createMicrophone() throws MicrophoneException {
     AudioQueueMicrophone mic = new AudioQueueMicrophone(48000, 960, null);
     mic.open();
     return mic;
 }
-```
-
 }
